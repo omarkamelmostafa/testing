de0f8fb9e64c5457c6bc67474bb182863979852b
Omar Kamel <iomarkamel@gmail.com>
2023-11-03 07:01:47 +0200

    initial commit message

---
+++ app.js
@@ -0,0 +1,15 @@
+import { handleScrapping } from './lib/scraper.js';
+import { writeExcel, writeJson } from "./utils/index.js";
+
+const main = async () => {
+  try {
+    const data = await handleScrapping();
+
+    await writeExcel(data);
+    await writeJson(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+main();
